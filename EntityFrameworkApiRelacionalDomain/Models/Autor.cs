8e942ec4ccb8981fb49e48a9612e03ebe413ea56
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2020-11-17 19:40:59 -0300

    EFWebApi

--- EntityFrameworkApiRelacionalDomain/Models/Autor.cs
+++ EntityFrameworkApiRelacionalDomain/Models/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,17 @@ namespace EntityFrameworkApiRelacional.Models
     public class Autor
     {
         public int AutorId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório !")]
+        [Display(Name ="Nome")]
+        [StringLength(100, MinimumLength =3, ErrorMessage ="Entre 03 e 100 Caracteres")]
         public String Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório !")]
+        [Display(Name = "Email")]
+        [StringLength(100, ErrorMessage = "Campo Até 100 Caracteres")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Email com formato Incorreto !")]
         public String Email { get; set; }
 
         public ICollection <Livro> Livros { get; set; }
--- EntityFrameworkApiRelacionalDomain/Models/Livro.cs
+++ EntityFrameworkApiRelacionalDomain/Models/Livro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,15 @@ namespace EntityFrameworkApiRelacional.Models
     public class Livro
     {
         public int LivroId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório !")]
+        [Display(Name = "Titulo")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Entre 01 e 100 Caracteres")]
         public String Titulo { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório !")]
+        [Display(Name = "Valor")]
+        [DataType(DataType.Currency)]
         public Decimal? Valor { get; set; }
 
         public Autor Autor { get; }
